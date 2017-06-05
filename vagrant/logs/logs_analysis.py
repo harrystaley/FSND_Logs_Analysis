94e77c10a015c2cc14a1031c48e5ab6eb185604a
harrystaley <staleyh@gmail.com>
2017-06-05 02:31:49 -0500

    finished out functions for error rates

--- vagrant/logs/logs_analysis.py
+++ vagrant/logs/logs_analysis.py
@@ -7,7 +7,11 @@ def connect():
 
 
 def top_articles():
-    """FInds the top three articles."""
+    """
+    FInds the top three articles and displays them in descending 
+    order by article popularity as defined by the number of times an article
+    has been visited.
+    """
     DB = connect()
     c = DB.cursor()
     c.execute("SELECT title, COUNT(ip) AS visits " +
@@ -18,10 +22,59 @@ def top_articles():
     DB.close()
     return articles
 
-articles = top_articles()
 
-print "1. What are the most popular three articles of all time? \n"
+def top_authors():
+    """
+    queries the authors and displays them in descending
+    order by popularityas defined by the number of times an article
+    has been visited.
+    """
+    DB = connect()
+    c = DB.cursor()
+    c.execute("SELECT name, COUNT(ip) AS visits " +
+              "FROM article_log_view GROUP BY name " +
+              "ORDER BY visits DESC")
+    DB.commit()
+    authors = c.fetchall()
+    DB.close()
+    return authors
+
+
+def top_error_rates():
+    """
+    Identifies what days had greater than 1% of errors occour based on
+    http status codes.
+    """
+    DB = connect()
+    c = DB.cursor()
+    c.execute("SELECT * FROM error_rates_view WHERE rate >= 1")
+    DB.commit()
+    error_rates = c.fetchall()
+    DB.close()
+    return error_rates
+
+# DISPLAY RESULTS OF QUERIES
+
+articles = top_articles()
+authors = top_authors()
+errors = top_error_rates()
+
+# TOP 3 ARTICLES
+print "1. What are the most popular three articles of all time?"
+print "NOTE: Based upon the number of times an article has been accessed.\n"
 for article in articles:
     print article[0] + " -- " + str(article[1]) + " views"
-
 print "\n"
+
+# TOP AUTHORS
+print "2. Who are the most popular article authors of all time? "
+print "NOTE: Based upon the number of times an article has been accessed.\n"
+for author in authors:
+    print author[0] + " -- " + str(author[1]) + " views"
+print "\n"
+
+# HIG ERROR RATES
+print "3. On which days did more than 1% of requests lead to errors?\n"
+for error in errors:
+    print error[0] + " -- " + str(error[1]) + "%"
+
--- vagrant/logs/setup.sql
+++ vagrant/logs/setup.sql
@@ -18,3 +18,17 @@ CREATE OR REPLACE VIEW article_log_view AS
 	FROM article_author_join_view
 	LEFT OUTER JOIN log
 	ON log.path LIKE '%'||article_author_join_view.slug;
+
+CREATE OR REPLACE VIEW errors_per_day_view AS
+	SELECT date_trunc('day', time) AS date, 
+	COUNT(status) AS status_count,
+	SUM(CASE WHEN status LIKE '%404%' THEN 1 ELSE 0 END) AS errors
+	FROM log
+	GROUP BY date;
+
+CREATE OR REPLACE VIEW error_rates_view AS
+	SELECT TO_CHAR(date, 'MM/DD/YYYY') AS date,
+	ROUND(errors::decimal / status_count::decimal * 100, 2) AS rate
+	FROM errors_per_day_view
+	GROUP BY date, errors, status_count
+	ORDER BY rate DESC;
