d923a9555326fd32ee86c151158f40c11bfdfc5b
harrystaley <staleyh@gmail.com>
2017-06-04 22:55:43 -0500

    Formatted output for first question to be answered.

--- vagrant/logs/logs_analysis.py
+++ vagrant/logs/logs_analysis.py
@@ -10,13 +10,18 @@ def top_articles():
     """FInds the top three articles."""
     DB = connect()
     c = DB.cursor()
-    c.execute("SELECT title, COUNT(ip) AS visits FROM article_log_view GROUP BY visits;")
+    c.execute("SELECT title, COUNT(ip) AS visits " +
+              "FROM article_log_view GROUP BY title " +
+              "ORDER BY visits DESC LIMIT 3")
     DB.commit()
     articles = c.fetchall()
     DB.close()
     return articles
 
 articles = top_articles()
-print "1. What are the most popular three articles of all time?"
-print articles
 
+print "1. What are the most popular three articles of all time? \n"
+for article in articles:
+    print article[0] + " -- " + str(article[1]) + " views"
+
+print "\n"
